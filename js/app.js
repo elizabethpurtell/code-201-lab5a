079e9a9a7ef2430440b173109c0beddd7434246e
elizabethpurtell <epurtell.CF23@gmail.com>
2023-08-14 10:53:42 -0700

    Problem1 completed lab05a

--- js/app.js
+++ js/app.js
@@ -8,12 +8,22 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+// function sum(a, b) { //eslint-disable-line
 
+// }
+
+function sum(a, b) {
+  const total = a + b;
+  const resultString = `The sum of ${a} and ${b} is ${total}.`;
+
+  return [total, resultString];
 }
 
+const result = sum(4, 7);
+console.log(result); // This will output: [11, "The sum of 4 and 7 is 11."]
+
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+sum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -116,7 +126,7 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
