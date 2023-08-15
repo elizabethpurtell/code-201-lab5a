db38e71eb5d68b968a4c96a181bebc9bb0259da0
elizabethpurtell <epurtell.CF23@gmail.com>
2023-08-14 18:51:44 -0700

    fixed problem3 and all 3 pass now

--- js/app.js
+++ js/app.js
@@ -8,9 +8,6 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a, b) { //eslint-disable-line
-
-// }
 
 function sum(a, b) {
   let mySum = a + b;
@@ -19,11 +16,9 @@ function sum(a, b) {
   return [mySum, myStr];
 }
 
-// const result = sum(4, 7);
-// console.log(result); // This will output: [11, "The sum of 4 and 7 is 11."]
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -36,7 +31,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b) {
   let myProduct = a * b;
   let myresultString = `The product of ${a} and ${b} is ${myProduct}.`;
 
@@ -45,7 +40,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5, 9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -63,21 +58,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 
-function sum(a, b) {
-  return a + b;
-}
+function sumAndMultiply(num1, num2, num3) {
+  let [sumResult, sumString] = sum(sum(num1, num2)[0], num3);
+  let [productResult, productString] = multiply(multiply(num1, num2)[0], num3);
+  let combinedSumString = `${num1} and ${num2} and ${num3} sum to ${sumResult}.`;
+  let combinedProductString = `The product of ${num1} and ${num2} and ${num3} is ${productResult}.`;
 
-function multiply(a, b) {
-  return a * b;
-}
-
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-  let totalSum = sum(sum(a, b), c);
-  let totalProduct = multiply(multiply(a, b), c);
-  let sumString = `${a} and ${b} and ${c} sum to ${totalSum}.`;
-  let productString = `The product of ${a} and ${b} and ${c} is ${totalProduct}.`;
-
-  return [totalSum, totalProduct, sumString, productString];
+  return [sumResult, productResult, combinedSumString, combinedProductString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
